54aa6d83c255bd7da193a7a7174a5c7c0806466a
xiejesses <835612575@qq.com>
2018-09-28 17:46:03 +0800

    个人中心页布局

---
+++ musicsoul/components/login-button/index.js
@@ -0,0 +1,36 @@
+// components/login-button/index.js
+Component({
+  options: {
+    multipleSlots: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    openType: {
+      type: String
+    },
+    imageSrc: {
+      type: String
+    },
+    bindgetuserinfo: {
+      type: String
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onGetUserInfo(e) {
+      this.triggerEvent('getuserinfo', e.detail, {})
+    }
+  }
+})
